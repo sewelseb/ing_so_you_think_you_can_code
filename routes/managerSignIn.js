d4071eb1305443cb0c3e592c82b002a9da363ccc
sewelseb <sewelseb@hotmail.com>
2015-03-28 16:35:11 +0100

    ajout de login manager

---
+++ routes/managerSignIn.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET INGManager homepage page. */
+router.get('/', function(req, res, next) {
+  res.render('manager', { title: 'Express' });
+});
+
+module.exports = router;
