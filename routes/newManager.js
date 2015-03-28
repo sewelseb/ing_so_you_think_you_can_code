63174770fd331ebd613384be0ffc35033e9f65a8
sewelseb <sewelseb@hotmail.com>
2015-03-28 18:13:40 +0100

    test de mongoose

--- routes/newManager.js
+++ routes/newManager.js
@@ -1,5 +1,5 @@
 var express = require('express');
-//var mongo = require('mongodb');
+var mongo = require('mongodb');
 //var mongoose = require('mongoose');
 
 var router = express.Router();
@@ -7,10 +7,12 @@ var router = express.Router();
 
 //mongoose.connect('159.8.128.87', 'INGVector');
 //mongoose.connect('mongodb://username:9c92fbaa-ab9b-41ce-9327-364fd9d00115@159.8.128.87:10563/ingvestor');
+//db.on('error', console.log('connection error'));
+
 
 router.post('/', function(req, res, next) {
   // var db = mongoose.connection;
-  // //db.on('error', console.log('connection error'));
+  
   // console.log(req.post.lastname+" "+req.post.firstname);
   // var manager= mongoose.Schema({
   // 				lastname: req.post.lastname, 
