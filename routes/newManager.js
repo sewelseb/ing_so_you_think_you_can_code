3f554175b51b4ef10f6b7fa6874dd92b0e359aca
sewelseb <sewelseb@hotmail.com>
2015-03-28 18:09:36 +0100

    ajout d'un mongoose stable

--- routes/newManager.js
+++ routes/newManager.js
@@ -1,23 +1,23 @@
 var express = require('express');
-var mongo = require('mongodb');
-var mongoose = require('mongoose');
+//var mongo = require('mongodb');
+//var mongoose = require('mongoose');
 
 var router = express.Router();
 
 
 //mongoose.connect('159.8.128.87', 'INGVector');
-mongoose.connect('mongodb://username:9c92fbaa-ab9b-41ce-9327-364fd9d00115@159.8.128.87:10563/ingvestor');
+//mongoose.connect('mongodb://username:9c92fbaa-ab9b-41ce-9327-364fd9d00115@159.8.128.87:10563/ingvestor');
 
 router.post('/', function(req, res, next) {
-  var db = mongoose.connection;
-  //db.on('error', console.log('connection error'));
-  console.log(req.post.lastname+" "+req.post.firstname);
-  var manager= mongoose.Schema({
-  				lastname: req.post.lastname, 
-  				firtsname: req.post.firstname
-  			});
+  // var db = mongoose.connection;
+  // //db.on('error', console.log('connection error'));
+  // console.log(req.post.lastname+" "+req.post.firstname);
+  // var manager= mongoose.Schema({
+  // 				lastname: req.post.lastname, 
+  // 				firtsname: req.post.firstname
+  // 			});
 			
-  console.log('test');
+  // console.log('test');
   res.end('register');
 });
 
