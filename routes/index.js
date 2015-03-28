7e8b2f0cada69fdae96cc70b3f517794c0e6433a
sheplu <jean.burellier@gmail.com>
2015-03-28 11:27:56 +0100

    Let it go!

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/demoboot', function(req, res, next) {
+  res.render('demoboot', { title: 'Express' });
+});
+
 module.exports = router;
