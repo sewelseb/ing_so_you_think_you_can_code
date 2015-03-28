505ee74460691ed8148b3e30a75deb55f71d3fd1
sheplu <jean.burellier@gmail.com>
2015-03-28 18:09:59 +0100

    remove mongoose and adding session support

--- routes/newManager.js
+++ routes/newManager.js
@@ -1,12 +1,7 @@
 var express = require('express');
 var mongo = require('mongodb');
-var mongoose = require('mongoose');
 
 var router = express.Router();
-
-
-//mongoose.connect('159.8.128.87', 'INGVector');
-mongoose.connect('mongodb://username:9c92fbaa-ab9b-41ce-9327-364fd9d00115@159.8.128.87:10563/ingvestor');
 
 router.post('/', function(req, res, next) {
   var db = mongoose.connection;
